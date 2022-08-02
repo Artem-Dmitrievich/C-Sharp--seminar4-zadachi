e323467452cae8d92205219e1a557a7bb7f70c79
kozlov artem <artemko1987@yandex.ru>
2022-08-02 20:37:20 +0300

    доп задача

---
+++ Задача1/Program.cs
@@ -0,0 +1,19 @@
+﻿// 
+int n = Convert.ToInt32(Console.ReadLine());
+int max_first = n;
+int max_second = -1;
+while (n != 0)
+{
+    n = Convert.ToInt32(Console.ReadLine());
+    if (n > max_first)
+    {
+        max_second = max_first;
+        max_first = n;
+    }
+    else
+    {
+        if (n > max_second)
+            max_second = n;
+    }
+}
+Console.WriteLine(max_second);
